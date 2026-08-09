86d929d130e6451ca96a0a98e1a01eef6709300a
thithipetit-hash <thithipetit@gmail.com>
2026-08-09 10:05:30 +0200

    Met à jour la version du groupe E

--- frontend/src/release-version-enhancements.js
+++ frontend/src/release-version-enhancements.js
@@ -7,9 +7,9 @@
  * - jj : jour ;
  * - iii : index du commit sur trois chiffres, incrémenté à chaque commit.
  *
- * La version de ce commit est donc 260809.074.
+ * La version de ce commit est donc 260809.080.
  */
-const APP_VERSION = "260809.074";
+const APP_VERSION = "260809.080";
 const APP_VERSION_LABEL = `Version : ${APP_VERSION}`;
 let scheduled = false;
 
