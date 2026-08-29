324f0bf2d4e5abb95225a332b7ac71dfc96e667f
thithipetit-hash <thithipetit@gmail.com>
2026-08-29 22:44:04 +0200

    test: cover default session status rule

---
+++ backend/test/session-default-status.test.js
@@ -0,0 +1,13 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { getDefaultSessionStatus } from "../session-default-status.js";
+
+test("mardi midi et jeudi midi sont encadrés par défaut", () => {
+  assert.equal(getDefaultSessionStatus("2026-09-01", "midi"), "encadree");
+  assert.equal(getDefaultSessionStatus("2026-09-03", "midi"), "encadree");
+});
+
+test("les autres créneaux sont libres par défaut", () => {
+  assert.equal(getDefaultSessionStatus("2026-09-01", "soir"), "libre");
+  assert.equal(getDefaultSessionStatus("2026-09-02", "midi"), "libre");
+});
