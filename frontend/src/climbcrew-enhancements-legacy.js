536572469e7375b4dcc381c3df548365ba99b8ad
thithipetit-hash <thithipetit@gmail.com>
2026-08-29 08:33:37 +0200

    chore(frontend): préparer migration des ajustements DOM

---
+++ frontend/src/climbcrew-enhancements-legacy.js
@@ -0,0 +1,6 @@
+/**
+ * Copie figée de l'ancien moteur d'ajustements DOM.
+ * Chargée par climbcrew-enhancements.js avec les MutationObserver neutralisés,
+ * afin de conserver le rendu actuel pendant la migration vers React natif.
+ */
+export * from "./climbcrew-enhancements.js";
