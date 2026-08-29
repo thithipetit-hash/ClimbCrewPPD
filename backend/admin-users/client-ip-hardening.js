2cf3bb53fc1c04d19b6774575c3b7cbb94f03e8f
thithipetit-hash <thithipetit@gmail.com>
2026-08-29 19:23:48 +0200

    refactor: expose trusted client IP middleware directly

--- backend/admin-users/client-ip-hardening.js
+++ backend/admin-users/client-ip-hardening.js
@@ -1,8 +1,4 @@
-import express from "express";
 import { CANONICAL_RATE_LIMIT_IP } from "./prebody-rate-limit.js";
-
-const EXPRESS_CLIENT_IP_PATCH = Symbol.for("climbcrew.client-ip-patch");
-const APP_CLIENT_IP_MIDDLEWARE = Symbol.for("climbcrew.client-ip-middleware");
 
 /**
  * Réécrit les en-têtes d'adresse à partir de req.ip, donc après application de
@@ -24,18 +20,4 @@ export function trustedClientIpMiddleware(req, _res, next) {
     delete req.headers["x-real-ip"];
   }
   next();
-}
-
-export function installClientIpHardening() {
-  if (express.application[EXPRESS_CLIENT_IP_PATCH]) return;
-  express.application[EXPRESS_CLIENT_IP_PATCH] = true;
-
-  const originalUse = express.application.use;
-  express.application.use = function patchedUseWithTrustedClientIp(...handlers) {
-    if (!this[APP_CLIENT_IP_MIDDLEWARE]) {
-      originalUse.call(this, trustedClientIpMiddleware);
-      this[APP_CLIENT_IP_MIDDLEWARE] = true;
-    }
-    return originalUse.apply(this, handlers);
-  };
 }
