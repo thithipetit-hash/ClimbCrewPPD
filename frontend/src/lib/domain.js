34f3d41823c4e5171d72335880045c547c4c1a69
thithipetit-hash <thithipetit@gmail.com>
2026-08-30 17:49:02 +0200

    refactor: import shared session status directly in frontend

--- frontend/src/lib/domain.js
+++ frontend/src/lib/domain.js
@@ -5,7 +5,7 @@ import {
   isSuccessfulLeadRealisation,
   isSuccessfulRealisation,
 } from "./realisation-mode.js";
-export { getDefaultSessionStatus as defaultSessionStatus } from "../../../backend/session-default-status.js";
+export { getDefaultSessionStatus as defaultSessionStatus } from "../../../shared/session-default-status.js";
 
 export const GRADES = ["4","4a","4b","4c","5a","5a+","5b","5b+","5c","5c+","6a","6a+","6b","6b+","6c","6c+","7a","7a+","7b","7c"];
 
